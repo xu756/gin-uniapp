c46db863b8bc5ee5d3b877141dc8d2b3b451d55b
xu756 <756334744@qq.com>
2022-07-07 22:50:19 +0800

    init

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,17 +1,98 @@
 <template>
-  <view class="content">
-    <view>
-      <text class="title">{{ title }}</text>
+  <view class="M_b">
+    <view class="M_h">
+      <view class="M_h_title"> 新媒体工具 V1.0.1 </view>
+      <view class="M_h_container">
+        <view class="M_h_box">
+          <view class="p1">
+            <svg
+              t="1657188967035"
+              class="icon"
+              viewBox="0 0 1024 1024"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              p-id="4309"
+              width="40"
+              height="40"
+            >
+              <path
+                d="M869.12 1024a151.04 151.04 0 0 1-106.88-44.16L486.4 704A360.32 360.32 0 0 1 26.24 232.96a56.96 56.96 0 0 1 42.24-35.84 57.6 57.6 0 0 1 53.76 16l140.8 140.8 90.24-90.24-140.8-140.8a59.52 59.52 0 0 1-16-53.76 59.52 59.52 0 0 1 36.48-42.24 360.32 360.32 0 0 1 484.48 275.2A355.84 355.84 0 0 1 704 487.04l275.2 275.84a151.04 151.04 0 0 1-106.88 256zM506.88 606.08l19.2 22.4 3.84 3.84 288 288.64a72.32 72.32 0 0 0 102.4-102.4L631.68 529.92h-3.84l-22.4-19.84 12.8-26.88a280.96 280.96 0 0 0-325.76-392.32l172.8 172.8-202.24 202.24-172.8-172.8a280.96 280.96 0 0 0 389.12 325.12z m0 0"
+                fill="#ffffff"
+                p-id="4310"
+              ></path>
+            </svg>
+          </view>
+          <view class="p2"> 500 </view>
+          <view class="p3"> 工具数量 </view>
+        </view>
+        <view class="M_h_box">
+          <view class="p1">
+            <svg
+              t="1657189305137"
+              class="icon"
+              viewBox="0 0 1024 1024"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              p-id="6645"
+              width="40"
+              height="40"
+            >
+              <path
+                d="M979.2 158.848a32 32 0 0 0-38.4-23.936l-166.848 38.656a32 32 0 0 0-15.808 53.408L794.56 264.64l-194.56 173.568-101.024-95.68a32 32 0 0 0-45.152 1.152l-216.736 227.264A64 64 0 1 0 288 633.6c0-6.944-1.376-13.472-3.424-19.712l193.536-202.944 99.232 93.952a32 32 0 0 0 43.296 0.64L839.04 310.72l41.504 42.976a32 32 0 0 0 53.92-13.92l44.448-165.408a31.84 31.84 0 0 0 0.288-15.52z"
+                p-id="6646"
+                fill="#ffffff"
+              ></path>
+              <path
+                d="M928 450.464a32 32 0 0 0-32 32V736a32 32 0 0 1-32 32H160a32 32 0 0 1-32-32V160a32 32 0 0 1 32-32h530.656a32 32 0 0 0 0-64H160a96 96 0 0 0-96 96v576a96 96 0 0 0 96 96h704a96 96 0 0 0 96-96v-253.536a32 32 0 0 0-32-32zM912 896h-800a32 32 0 0 0 0 64h800a32 32 0 0 0 0-64z"
+                p-id="6647"
+                fill="#ffffff"
+              ></path>
+            </svg>
+          </view>
+          <view class="p2"> 50015 </view>
+          <view class="p3"> 使用次数 </view></view
+        >
+        <view class="M_h_box">
+          <view class="p1">
+            <svg
+              t="1657189231371"
+              class="icon"
+              viewBox="0 0 1024 1024"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              p-id="4738"
+              width="40"
+              height="40"
+            >
+              <path
+                d="M864 280.224V192a32 32 0 0 0-32-32H160a32 32 0 0 0-32 32v88.224h736z m-736 64V864a32 32 0 0 0 32 32h672a32 32 0 0 0 32-32v-86.592a32 32 0 1 1 64 0V864a96 96 0 0 1-96 96H160a96 96 0 0 1-96-96V192a96 96 0 0 1 96-96h672a96 96 0 0 1 96 96v120.224a32 32 0 0 1-32 32H128z"
+                p-id="4739"
+                fill="#ffffff"
+              ></path>
+              <path
+                d="M595.84 437.088a32 32 0 0 1 60.16 21.856l-109.44 288a32 32 0 0 1-60.16-21.856l109.44-288zM744.192 469.408a32 32 0 1 1 47.616-42.816l126.432 140.64a32 32 0 0 1 0 42.784l-126.432 140.64a32 32 0 0 1-47.616-42.784l107.232-119.232-107.2-119.232zM406.24 469.408a32 32 0 1 0-47.584-42.816l-126.464 140.64a32 32 0 0 0 0 42.784l126.464 140.64a32 32 0 0 0 47.584-42.784l-107.2-119.232 107.2-119.232z"
+                p-id="4740"
+                fill="#ffffff"
+              ></path>
+            </svg>
+          </view>
+          <view class="p2"> 80000</view>
+          <view class="p3"> Github源代码 </view></view
+        >
+      </view>
     </view>
+    <view class=".M_c"> </view>
   </view>
 </template>
 
 <script>
+import exDrawer from "@/components/ex-drawer/ex-drawer.vue";
 export default {
+  components: {
+    exDrawer,
+  },
   data() {
-    return {
-      title: "Hello",
-    };
+    return {};
   },
   onLoad() {},
   methods: {},
@@ -19,4 +100,34 @@ export default {
 </script>
 
 <style lang="scss">
+.M_b {
+  width: 100%;
+  background-color: #fff;
+  .M_h {
+    width: 100%;
+    height: 360rpx;
+    background: linear-gradient(90deg, #ff512f 0%, #f09819 100%);
+    .M_h_title {
+      width: 100%;
+      padding-top: 50rpx;
+      text-align: center;
+      font-weight: 700;
+      color: #fff;
+      font-size: 30rpx;
+    }
+    .M_h_container {
+      padding-top: 50rpx;
+      width: 80%;
+      margin: 0 auto;
+      display: flex;
+      justify-content: space-around;
+      text-align: center;
+      color: #fff;
+      font-size: 22rpx;
+    }
+  }
+  .M_c {
+    width: 100%;
+  }
+}
 </style>
